f2a3ca39d2300ba5f94a58a5c8d5c816e8eec418
lucasz10 <lucas.zach10@gmail.com>
2022-09-12 21:32:45 -0700

    Created routing for saving notes and getting notes

---
+++ routes/routes.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const router = require('express').Router();
+
+// Retrieves notes information to display on screen
+router.get('/notes', (req,res) => {
+    console.log("Getting previous notes...");
+    fs.readFile('./db/db.json', 'utf8', (err,data) => {
+        if (err) {
+            console.error(err)
+        } else {
+            return res.json(JSON.parse(data));
+        }
+    })
+})
+
+// Adds new note
+router.post('/notes', (req,res) => {
+    console.log("Notes being updated...");
+    fs.readFile('./db/db.json', 'utf8', (err,data) => {
+        let notes = JSON.parse(data);
+
+        const {title, text} = req.body;
+
+        if(req.body) {
+            const newNote = {
+                title,
+                text,
+            };
+            notes.push(newNote);
+
+            fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    res.json(notes);
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const api = require('./routes/routes.js');
 
+// Initialize Express
 const PORT = 3001;
-
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api)
 app.use(express.static('public'));
 
+// Routing
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -16,11 +22,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//TODO: GET /api/notes
-
-
-//TODO: POST /api/notes
-
+// Initiates port listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
